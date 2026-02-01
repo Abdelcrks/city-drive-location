1e02c2fb6ba84ef2465948e60851b385fab4f115
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-02-01 11:53:40 +0100

    initial svelte kit setup layout navbar and footer text

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<nav>
+<h1>City Drive</h1>
+<a href="/">Accueil</a>
+<a href="/cars">Nos véhicules</a>
+<a href="/contacts">Nous contacter</a>
+</nav>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+
+{@render children()}
+
+
+<footer>
+	<h1>© City Drive – Location de voitures</h1>
+	<button>Whatsapp</button>
+	<button>Snapchat</button>
+</footer>
