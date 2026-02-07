210135de8fed9cc51d8078796adb69a52e12fb0d
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-02-07 13:39:11 +0100

    feat add cars/id page description images etc

--- src/lib/data/cars.ts
+++ src/lib/data/cars.ts
@@ -7,10 +7,11 @@ export type Car = {
 	year: number;
 	pricePerDay?: number; 
 	transmission: 'auto' | 'manual';
-	fuel?: 'diesel' | 'petrol' | 'hybrid' | 'electric';
+	fuel?: 'diesel' | 'essence' | 'hybrid' | 'electric';
 	color: string;
 	image: string;
 	description: string;
+	images: string[]
 };
 
 export const cars: Car[] = [
@@ -22,11 +23,13 @@ export const cars: Car[] = [
         category: 'sport',
 		year: 2020,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Gris Designo / Olive Metallic',
+		pricePerDay: 300,
 		image: '/cars/classe-g63/avg.jpg',
 		description:
-			'Mercedes Classe G63 AMG V8 Biturbo, un SUV de prestige au caractère affirmé. Intérieur cuir AMG, jantes 22 pouces et échappement AMG pour une expérience unique.'
+			'Mercedes Classe G63 AMG V8 Biturbo, un SUV de prestige au caractère affirmé. Intérieur cuir AMG, jantes 22 pouces et échappement AMG pour une expérience unique.',
+		images : ["/cars/classe-g63/avg.jpg","/cars/classe-g63/avd.jpg","/cars/classe-g63/cote.jpg", "/cars/classe-g63/arg.jpg", "/cars/classe-g63/ard.jpg","/cars/classe-g63/interieur.avif"]
 	},
 
 	{
@@ -37,11 +40,12 @@ export const cars: Car[] = [
 		model: 'Classe A',
 		year: 2025,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Noir brillant',
 		image: '/cars/classe-a/avg.jpg',
 		description:
-			'Mercedes Classe A 2025 en finition élégante. Compacte, moderne et confortable, idéale pour la ville comme pour les longs trajets.'
+			'Mercedes Classe A 2025 en finition élégante. Compacte, moderne et confortable, idéale pour la ville comme pour les longs trajets.',
+		images: ["/cars/classe-a/avg.jpg","/cars/classe-a/avd.jpg","/cars/classe-a/cote.jpg","/cars/classe-a/arg.jpg","/cars/classe-a/ard.jpg","/cars/classe-a/interieur.jpg","/cars/classe-a/avant.jpg","/cars/classe-a/arriere.jpg",]
 	},
 
 	{
@@ -52,11 +56,12 @@ export const cars: Car[] = [
 		model: 'Série 1 M Sport Pro',
 		year: 2025,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Gris Nardo',
 		image: '/cars/bmw-serie-1/avd.jpg',
 		description:
-			'BMW Série 1 Pack M Sport Pro au look sportif et dynamique. Finition haut de gamme, tenue de route précise et design affirmé.'
+			'BMW Série 1 Pack M Sport Pro au look sportif et dynamique. Finition haut de gamme, tenue de route précise et design affirmé.',
+		images: ["/cars/bmw-serie-1/avd.jpg","/cars/bmw-serie-1/ard.jpeg","/cars/bmw-serie-1/interieur.jpg","/cars/bmw-serie-1/cote.jpg","/cars/bmw-serie-1/arriere.jpg","/cars/bmw-serie-1/avant.jpeg","/cars/bmw-serie-1/arg.jpg","/cars/bmw-serie-1/feuav.jpg",]
 	},
 
 	{
@@ -67,11 +72,12 @@ export const cars: Car[] = [
 		model: 'Polo 6 R-Line',
 		year: 2024,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Noir brillant',
 		image: '/cars/polo-6.jpg',
 		description:
-			'Volkswagen Polo 6 R-Line 2024, sportive et élégante. Parfaite pour une conduite urbaine confortable avec un style dynamique.'
+			'Volkswagen Polo 6 R-Line 2024, sportive et élégante. Parfaite pour une conduite urbaine confortable avec un style dynamique.',
+		images:["/cars/polo-6.jpg"]
 	},
 
 	{
@@ -82,11 +88,12 @@ export const cars: Car[] = [
 		model: '208 GT',
 		year: 2022,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Bleu',
 		image: '/cars/peugeot-208.jpg',
 		description:
-			'Peugeot 208 GT au design moderne et sportif. Compacte, agile et agréable à conduire, idéale pour le quotidien.'
+			'Peugeot 208 GT au design moderne et sportif. Compacte, agile et agréable à conduire, idéale pour le quotidien.',
+		images:["/cars/peugeot-208.jpg"]
 	},
 
 	{
@@ -101,7 +108,9 @@ export const cars: Car[] = [
 		color: 'Gris métallisé foncé',
 		image: '/cars/clio-5-hybrid.jpg',
 		description:
-			'Renault Clio 5 Hybrid alliant économie et confort. Parfaite pour une conduite fluide et silencieuse en ville.'
+			'Renault Clio 5 Hybrid alliant économie et confort. Parfaite pour une conduite fluide et silencieuse en ville.',
+			images:["/cars/clio-5-hybrid.jpg"]
+
 	},
 
 	{
@@ -112,11 +121,12 @@ export const cars: Car[] = [
         category: 'citadine',
 		year: 2019,
 		transmission: 'manual',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Bleu nuit',
 		image: '/cars/clio-5-bleu.jpg',
 		description:
-			'Renault Clio 5 boîte manuelle en bleu nuit. Véhicule fiable et polyvalent, idéal pour tous types de trajets.'
+			'Renault Clio 5 boîte manuelle en bleu nuit. Véhicule fiable et polyvalent, idéal pour tous types de trajets.',
+		images:["/cars/clio-5-bleu.jpg"]
 	},
 
 	{
@@ -131,7 +141,9 @@ export const cars: Car[] = [
 		color: 'Gris',
 		image: '/cars/yaris-cross.jpg',
 		description:
-			'Toyota Yaris Cross hybride, SUV compact moderne et économique. Confort et fiabilité pour un usage quotidien.'
+			'Toyota Yaris Cross hybride, SUV compact moderne et économique. Confort et fiabilité pour un usage quotidien.',
+			images:["/cars/yaris-cross.jpg"]
+
 	},
 
 	{
@@ -142,11 +154,13 @@ export const cars: Car[] = [
         category: 'sport',
 		year: 2022,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Blanc',
 		image: '/cars/rs6.jpg',
 		description:
-			'Audi RS6, break ultra sportif combinant puissance et luxe. Un véhicule d’exception pour les amateurs de performance.'
+			'Audi RS6, break ultra sportif combinant puissance et luxe. Un véhicule d’exception pour les amateurs de performance.',
+			images:["/cars/yaris-cross.jpg"]
+
 	},
 
 	{
@@ -157,11 +171,13 @@ export const cars: Car[] = [
         category: 'sport',
 		year: 2024,
 		transmission: 'auto',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Noir mat',
 		image: '/cars/rs3.jpg',
 		description:
-			'Audi RS3 full option en noir mat. Sportivité extrême, design agressif et performances exceptionnelles.'
+			'Audi RS3 full option en noir mat. Sportivité extrême, design agressif et performances exceptionnelles.',
+			images:["/cars/yaris-cross.jpg"]
+
 	},
 
 	{
@@ -172,11 +188,13 @@ export const cars: Car[] = [
 		year: 2024,
 		transmission: 'auto',
         category: 'citadine',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Bleu Alpine',
 		image: '/cars/clio5-alpine-bleu/avg.webp',
 		description:
-			'Renault Clio 5 Alpine en bleu, finition sportive et élégante inspirée du savoir-faire Alpine.'
+			'Renault Clio 5 Alpine en bleu, finition sportive et élégante inspirée du savoir-faire Alpine.',
+			images:["/cars/clio5-alpine-bleu/avg.webp"]
+
 	},
 
 	{
@@ -187,11 +205,13 @@ export const cars: Car[] = [
 		year: 2024,
 		transmission: 'auto',
         category: 'citadine',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Gris',
 		image: '/cars/clio5-alpine-gris/avg.webp',
 		description:
-			'Renault Clio 5 Alpine gris, design racé et confort moderne pour une conduite dynamique.'
+			'Renault Clio 5 Alpine gris, design racé et confort moderne pour une conduite dynamique.',
+			images:["/cars/yaris-cross.jpg"]
+
 	},
     {
 		id: 'lamborghini-urus',
@@ -201,10 +221,12 @@ export const cars: Car[] = [
 		year: 2024,
 		transmission: 'auto',
         category: 'sport',
-		fuel: 'petrol',
+		fuel: 'essence',
 		color: 'Black',
 		image: '/cars/lamborghini-urus/avg.webp',
 		description:
-			" etc etc pas oublier d'ajouter la description"
+			" etc etc pas oublier d'ajouter la description",
+			images:["/cars/lamborghini-urus/avg.webp"]
+
 	},
 ]
